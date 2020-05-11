5c659e9a360126d05b89796a22a94b1b926a4c49
张晓斌 <18235442561@163.com>
2020-05-11 09:18:06 +0800

    Update TxtReader.java

--- io/src/main/java/com/yjqz/binge/io/TxtReader.java
+++ io/src/main/java/com/yjqz/binge/io/TxtReader.java
@@ -19,14 +19,14 @@ public class TxtReader {
 	@Test
 	public void readTxt() throws IOException {
 		File file = new File("C:\\Users\\zhangxiaobin\\Desktop\\temp\\fql.txt");
-		File targetFile = new File("C:\\Users\\zhangxiaobin\\Desktop\\temp\\B_CBIB_REPAY_FQL_FqlCCPay_20180625_001.txt");
+		File targetFile = new File("C:\\Users\\zhangxiaobin\\Desktop\\temp\\bbb.txt");
 		List<String> fileLines = FileUtils.readLines(file, "UTF-8");
 		List<String> head = new ArrayList<String>();
 		StringBuilder sb = new StringBuilder();
 		String headStr = sb.append("3").append("|")
 							.append("315.92").append("|")
-							.append("03002971517").append("|")
-							.append("51000631748").append("|")
+							.append("asas").append("|")
+							.append("asas").append("|")
 							.append("2018-06-25").toString();
 		head.add(headStr);
 		FileUtils.writeLines(targetFile, "UTF-8", head, IOUtils.LINE_SEPARATOR_UNIX, true);
